b6d14f76dae1748dc7bc7a14dc7bd8f6097619a0
yogieshwar <konneyogie@gmail.com>
2025-04-11 15:42:05 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './store/Store.js'
 import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
   <Provider store={store}>
      <App />
   </Provider>
-  </BrowserRouter>
+  </HashRouter>
    
  
 )
